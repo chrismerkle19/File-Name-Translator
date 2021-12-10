89b371fec330c5d93629b7011b04fb7ec2e145d5
chrismerkle19 <82123634+chrismerkle19@users.noreply.github.com>
2021-12-10 03:36:10 -0600

    Edit user prompts

--- translator.py
+++ translator.py
@@ -3,20 +3,29 @@ from os.path import exists
 from pathlib import Path
 from deep_translator import GoogleTranslator
 
+print(
+    "Do you want to translate the name of all files in this directory? \n(This may break programs that rely on the files being renamed)\n\n *CAN NOT UNDO! MAKE A BACKUP!* \n (y/n)\n")
 
-filepath = input("Enter Path of the file(s) to translate:\n")
-directory = Path(filepath).parent
-file = Path(filepath).name
-filename = Path(filepath).stem
-extension = Path(filepath).suffix
-translated = GoogleTranslator(source='auto', target='en').translate(filename)
+check = input("Continue? (y/n):\n")
 
+if check == ("y" or "Y"):
 
-if not exists(filepath):
-    print("File does not exist. Check inout and try again.")
+    entries = os.listdir(os.getcwd())
+
+    for entry in entries:
+        directory = Path(entry).parent
+        file = Path(entry).name
+        filename = Path(entry).stem
+        extension = Path(entry).suffix
+        translated = GoogleTranslator(source='auto', target='en').translate(filename)
+        file_translated = (translated + extension)
+
+        if not exists(file_translated):
+            os.rename(entry, file_translated)
+            print("Translated", entry, "to", file_translated)
+        else:
+            print("Skipping", entry, "(Might already be in English)")
+            continue
+    print("\n\n\nDone.")
 else:
-    newname = (translated + extension)
-    if not exists(newname):
-       os.rename(filepath, newname)
-    else:
-        print("File with specified name already exists, choose another name.\n")
+    exit()
